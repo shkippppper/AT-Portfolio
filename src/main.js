aa8896fe15399ec1ef85a69300fec3ab0f8cdf27
shkippppper <a.titberidze@yahoo.com>
2024-04-17 22:25:26 +0400

    Added router to the main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import './assets/styles/global.css';
+
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')

afe430ee2118020c8ffe4f036696da3f9b0f4f02
shkippppper <a.titberidze@yahoo.com>
2024-04-23 17:47:17 +0400

    Created dummy data

---
+++ src/models/ProjectData.js
@@ -0,0 +1,11 @@
+const projectData = [
+    { name: 'Card 1', slug: 'card-1', tags: ['Unity', 'C#'] },
+    { name: 'Card 2', slug: 'card-2', tags: ['SCSS', 'Vue.js'] },
+    { name: 'Card 10', slug: 'card-10', tags: ['PUG', 'Unity'] },
+    { name: 'Card 10', slug: 'card-10', tags: ['PUG', 'Unity'] },
+    { name: 'Card 10', slug: 'card-10', tags: ['Autodesk Inventor', 'SCSS'] },
+    { name: 'Card 10', slug: 'card-10', tags: ['3D Modelling', 'Autodesk Inventor'] },
+    { name: 'Card 10', slug: 'card-10', tags: ['Unity', '3D Modelling'] },
+];
+
+export default projectData

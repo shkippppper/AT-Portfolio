94a341ada3c3a1eab31866d0d4a63877e42d72f7
shkippppper <a.titberidze@yahoo.com>
2024-05-05 03:58:22 +0400

    updated projectdata

--- src/models/ProjectData.js
+++ src/models/ProjectData.js
@@ -2,10 +2,21 @@ const projectData = [
     { name: 'Card 1', slug: 'card-1', tags: ['Unity', 'C#'] },
     { name: 'Card 2', slug: 'card-2', tags: ['SCSS', 'Vue.js'] },
     { name: 'Card 10', slug: 'card-10', tags: ['PUG', 'Unity'] },
-    { name: 'Card 10', slug: 'card-10', tags: ['PUG', 'Unity'] },
-    { name: 'Card 10', slug: 'card-10', tags: ['Autodesk Inventor', 'SCSS'] },
-    { name: 'Card 10', slug: 'card-10', tags: ['3D Modelling', 'Autodesk Inventor'] },
-    { name: 'Card 10', slug: 'card-10', tags: ['Unity', '3D Modelling'] },
+    { name: 'Card 10', slug: 'card-11', tags: ['PUG', 'Unity'] },
+    { name: 'Card 10', slug: 'card-12', tags: ['Autodesk Inventor', 'SCSS'] },
+    { name: 'Card 10', slug: 'card-13', tags: ['3D Modelling', 'Autodesk Inventor'] },
+    { name: 'Card 10', slug: 'card-14', tags: ['Unity', '3D Modelling'] },
 ];
+
+// about-shi aris eseni
+// unity
+// c#
+// c++
+// inventor
+// autocad
+// vue.js
+// html
+// scss
+// pug
 
 export default projectData

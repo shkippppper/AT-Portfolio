e82c0e388f211c660045cfd9e5d7523d426ee133
shkippppper <a.titberidze@yahoo.com>
2024-05-05 03:59:19 +0400

    Added ProjectDetails.vue

---
+++ src/components/ProjectDetails.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+.project-details
+    h1 Project Details for {{ slug }}
+</template>
+
+<script>
+export default {
+    name: 'ProjectDetails',
+    props: {
+        slug: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>

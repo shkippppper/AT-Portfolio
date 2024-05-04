e5eb790146c58f6f2c0ff86691c5ee44544060e4
shkippppper <a.titberidze@yahoo.com>
2024-05-05 03:59:49 +0400

    Added new routes to the setup

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ProjectsLayout from '@/components/ProjectsLayout.vue'
 import BlogLayout from '@/components/BlogLayout.vue'
 import AboutLayout from '@/components/AboutLayout.vue'
+import ProjectDetails from "@/components/ProjectDetails.vue";
+import BlogDetails from "@/components/BlogDetails.vue";
 
 const routes = [
     { path: '/', component: ProjectsLayout, name: 'Projects' },
     { path: '/blog', component: BlogLayout, name: 'Blog' },
-    { path: '/about', component: AboutLayout, name: 'About' }
+    { path: '/about', component: AboutLayout, name: 'About' },
+    { path: '/project/:slug', component: ProjectDetails, name: 'ProjectDetails' },
+    { path: '/blog/:slug', component: BlogDetails, name: 'BlogDetails' },
 ]
 
 const router = createRouter({

84dc2b2ad4eeb4b7de66010f82569a0dcc91e716
shkippppper <a.titberidze@yahoo.com>
2024-05-05 03:58:30 +0400

    created blogdata

---
+++ src/models/BlogData.js
@@ -0,0 +1,8 @@
+const blogData = [
+    { name: 'Blog Card 1', slug: 'blog-card-1', tags: ['Unity', 'C#'] },
+    { name: 'Blog Card 2', slug: 'blog-card-2', tags: ['SCSS', 'Vue.js'] },
+    { name: 'Blog Card 10', slug: 'blog-card-10', tags: ['PUG', 'Unity'] },
+    { name: 'Blog Card 10', slug: 'blog-card-11', tags: ['PUG', 'Unity'] }
+];
+
+export default blogData

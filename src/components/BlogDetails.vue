818140a38f4b77738f7dda2468b94571582f145b
shkippppper <a.titberidze@yahoo.com>
2024-05-05 04:00:47 +0400

    Added blog details page

---
+++ src/components/BlogDetails.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+.blog-details
+    h1 Blog Details for {{ slug }}
+</template>
+
+<script>
+export default {
+    name: 'BlogDetails',
+    props: {
+        slug: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>

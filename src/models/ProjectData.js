69077f092e1be0449dbf1af96ea9fcb17e466986
shkippppper <a.titberidze@yahoo.com>
2024-05-07 01:15:13 +0400

    Created test file for correct display of cards

--- src/models/ProjectData.js
+++ src/models/ProjectData.js
@@ -1,5 +1,5 @@
 const projectData = [
-    { name: 'Card 1', slug: 'card-1', tags: ['Unity', 'C#'] },
+    { name: 'Echoes Of The Underhalls', slug: 'echoes-of-the-underhalls', tags: ['Unity', 'C#', 'Game Dev'] },
     { name: 'Card 2', slug: 'card-2', tags: ['SCSS', 'Vue.js'] },
     { name: 'Card 10', slug: 'card-10', tags: ['PUG', 'Unity'] },
     { name: 'Card 10', slug: 'card-11', tags: ['PUG', 'Unity'] },

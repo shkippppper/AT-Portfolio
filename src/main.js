46548e1c34a026ea78fb8f4bd177a28142b1cec6
shkippppper <a.titberidze@yahoo.com>
2024-05-16 00:23:54 +0400

    Added highlight js to the project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueHighlightJS from 'vue3-highlightjs'
+import 'highlight.js/styles/solarized-light.css'
 
 import './assets/styles/global.css';
 
@@ -9,3 +11,4 @@ import './assets/styles/global.css';
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+app.use(VueHighlightJS)
